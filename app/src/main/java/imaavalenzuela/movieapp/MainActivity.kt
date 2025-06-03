297472468f496f2d9e5881cc0279d764528a6a4e
ImaaValenzuela <imanolvalenzuela03@gmail.com>
2025-06-03 15:52:09 -0300

    Add Binding

--- app/src/main/java/imaavalenzuela/movieapp/MainActivity.kt
+++ app/src/main/java/imaavalenzuela/movieapp/MainActivity.kt
@@ -1,10 +1,11 @@
-package kotlin.kmp.imaavalenzuela.movieapp
+package imaavalenzuela.movieapp
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.kmp.imaavalenzuela.movieapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/imaavalenzuela/movieapp/MovieDetailFragment.kt
+++ app/src/main/java/imaavalenzuela/movieapp/MovieDetailFragment.kt
@@ -1,13 +1,17 @@
-package kotlin.kmp.imaavalenzuela.movieapp
+package imaavalenzuela.movieapp
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import kotlin.kmp.imaavalenzuela.movieapp.R
+import kotlin.kmp.imaavalenzuela.movieapp.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 }
