6f3201d4dca69b6860cfbceabb1ae41bf8b22512
ImaaValenzuela <imanolvalenzuela03@gmail.com>
2025-06-03 21:48:46 -0300

    Data

--- app/src/main/java/imaavalenzuela/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/imaavalenzuela/movieapp/data/remote/MovieDataSource.kt
@@ -1,4 +1,17 @@
 package imaavalenzuela.movieapp.data.remote
 
+import imaavalenzuela.movieapp.data.model.MovieList
+
 class MovieDataSource {
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList {
+        return MovieList()
+    }
 }
